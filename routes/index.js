5f0f6c01691fb1c96449f38356ea5149531df4dd
WentaoLLL <39769688+WentaoLLL@users.noreply.github.com>
2019-05-20 09:24:44 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,7 @@ router.post('/searchperson', function(req, res) {
     var collection = db.get('newcollection');
 
     // Submit to the DB
-    collection.findOne({
+    collection.find({
         "username" : userName,
         "usernachname" : userNachname
     }, function (err, doc) {
@@ -92,8 +92,9 @@ router.post('/searchperson', function(req, res) {
             //res.location("userlist");
             // And forward to success page
 	    res.render('searchresult', {
-            "namelist" : doc
+            "searchlist" : JSON.stringify(doc),
         });
+            //res.send(doc);
             res.redirect("searchresult");
         }
     });
